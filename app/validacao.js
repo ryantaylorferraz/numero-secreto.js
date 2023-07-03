929add06e42934b1b267a597fb58469f26d0837d
Ryan Taylor <ryan.bilac2010@hotmail.com>
2023-07-03 09:33:31 -0300

    project done

--- app/validacao.js
+++ app/validacao.js
@@ -14,7 +14,9 @@ function verificarSeOChutePossuiUmValorValido(chute) {
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
-        <h3>O número secreto era ${numeroSecreto}</h3>`
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class ="btn-jogar">Jogar novamente</button>`
+
     } else if(numero > numeroSecreto) {
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
@@ -33,4 +35,8 @@ function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero <  menorValor
 }
 
-//Próxima aula: criando as dicas
+document.body.addEventListener('click', e => {
+    if (e.target.id === 'jogar-novamente') {
+        window.location.reload()
+    }
+})
